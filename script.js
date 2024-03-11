dd221ec991b026926ff9f17225ed2bf11a63d28b
IliyanGeorgiev <iliangeorgiev300@yahoo.com>
2024-03-11 22:21:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const ratingComponent = document.querySelector(".rating-component");
+const thankyouComponent = document.querySelector(".thankyou-component");
+
+const ratingBtns = document.querySelectorAll(".btn");
+const rating = document.querySelector(".rating");
+
+const submitBtn = document.getElementById("submit-btn");
+const rateBtn = document.getElementById("rate-btn");
+
+
+
+submitBtn.addEventListener("click", () => {
+
+    thankyouComponent.classList.remove("hidden");
+    ratingComponent.style.display = "none";
+
+
+});
+
+
+rateBtn.addEventListener("click", () => {
+
+    thankyouComponent.classList.add("hidden");
+    ratingComponent.style.display = "block";
+
+});
+
+
+ratingBtns.forEach((rate) => {
+
+    rate.addEventListener("click", () => {
+
+        rating.innerHTML = rate.innerHTML;
+
+
+    })
+
+});
